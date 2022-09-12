196a3bc3aabb805127a36eb9c66db0d4fbb57ca2
Dave Matthew Peter Lumban Tobing <dave.matthew@ui.ac.id>
2022-09-12 21:03:14 +0700

    ngerjan tugas 2

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from xml.etree.ElementInclude import include
+from django.urls import path, include
 from example_app.views import index
 
 app_name = 'example_app'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('katalog/', include('katalog.urls')),
 ]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_Katalog = CatalogItem.objects.all()
+    context = {
+    'list_katalog': data_barang_Katalog,
+    'nama': 'Dave Matthew Peter Lumban Tobing',
+    'NPM' : '2106700870'
+    }
+    return render(request, "katalog.html", context)
+
