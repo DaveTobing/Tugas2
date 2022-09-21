560b7aef456d063790ce3bc232ab3758a8a1051e
Dave Matthew Peter Lumban Tobing <dave.matthew@ui.ac.id>
2022-09-21 16:16:58 +0700

    change urls

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -4,8 +4,7 @@ from mywatchlist.views import show_xml, show_json,show_html
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_html, name='show_html'), #sesuaikan dengan nama fungsi yang dibuat
+    path('html/', show_html, name='show_html'), #sesuaikan dengan nama fungsi yang dibuat
     path('xml/', show_xml, name='show_xml'), #sesuaikan dengan nama fungsi yang dibuat
     path('json/', show_json, name='show_json'), #sesuaikan dengan nama fungsi yang dibuat
 ]
-
